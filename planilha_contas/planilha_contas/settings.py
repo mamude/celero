428cbadd287fe7e223e4d9731921e84dfc5cf9c6
Samir Mamude <samir.mamude@ciatecnica.com.br>
2019-03-12 11:16:18 -0300

    Ajustado static_root

--- planilha_contas/planilha_contas/settings.py
+++ planilha_contas/planilha_contas/settings.py
@@ -131,4 +131,5 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
